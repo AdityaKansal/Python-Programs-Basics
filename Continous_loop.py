e4cf8ba584478b17d3f60961d1b0b92dd5b53f8c
Aditya Kansal <37431405+AdityaKansal@users.noreply.github.com>
2018-06-27 15:00:58 +0530

    Add files via upload

---
+++ Continous_loop.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 27 14:55:01 2018
+
+@author: akansal2
+"""
+while True:
+    message = input()
+    print(message)
+    if message.strip() == 'Bye':
+        break
